d971321b9ebc82638ea72c288894c4e6107d1a9c
mikemjharris <mike.mj.harris@gmail.com>
2016-11-30 21:16:51 +0000

    get slack token from the system

--- config/config.exs
+++ config/config.exs
@@ -28,3 +28,6 @@ use Mix.Config
 # here (which is why it is important to import them last).
 #
 #     import_config "#{Mix.env}.exs"
+
+config :elasta_bot, ElastaBot.Slack,
+  token: System.get_env("SLACK_TOKEN")

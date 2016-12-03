59393494e79d677c920fa279f32ce4b2f68c1b50
mikemjharris <mike.mj.harris@gmail.com>
2016-12-03 21:21:55 +0000

    refactor to make it more functional

--- lib/elasta_bot/query.ex
+++ lib/elasta_bot/query.ex
@@ -1,12 +1,26 @@
 defmodule ElastaBot.Query do
     def query_es() do
-        request = File.read!("./data/request.json") |> Poison.decode!()
-        request_json = Poison.Encoder.encode(request, [])
-        body = to_string request_json
-        elastic_search_url = Application.get_env(:elasta_bot, ElastaBot.Query)[:elastic_search_url]
-        results = HTTPotion.post elastic_search_url, [body: body,  ders: ["Content-Type": "application/json"]]
+        results = get_results_from_es()
         results = Poison.Parser.parse!(results.body) 
         alert = Enum.at(results["hits"]["hits"],1)["_source"]["message"] <> Enum.at(results["hits"]["hits"],2)["_source"]["message"] 
+    end
+
+    def get_results_from_es() do
+        elastic_search_url = Application.get_env(:elasta_bot, ElastaBot.Query)[:elastic_search_url]
+        
+        create_body_for_es_post_request()
+        |> (fn(body) -> HTTPotion.post elastic_search_url, [body: body,  headers: ["Content-Type": "application/json"]] end).()
+    end
+
+    def convert_to_json(map_to_convert) do
+        Poison.Encoder.encode(map_to_convert, [])
+    end
+
+    def create_body_for_es_post_request() do
+        File.read!("./data/request.json") 
+        |> Poison.decode!()
+        |> convert_to_json()
+        |> to_string
     end
 
 end

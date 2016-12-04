91e187329ea3f185691dcadae027144f921fec6d
mikemjharris <mike.mj.harris@gmail.com>
2016-12-04 18:18:08 +0000

    add the ability to set the number of queries

--- lib/elasta_bot/query.ex
+++ lib/elasta_bot/query.ex
@@ -1,15 +1,15 @@
 defmodule ElastaBot.Query do
     @request File.read!("./data/request.json") 
-    def query_es() do
-         get_results_from_es()
+    def query_es(nos_queries \\ 10) do
+         get_results_from_es(nos_queries)
          |> (fn(results) -> Poison.Parser.parse!(results.body)end).()
          |> (fn(r) -> r["hits"]["hits"] end).()
          |> Enum.reduce("", &(&2 <> "\n" <> &1["_source"]["message"]))
     end
 
-    def get_results_from_es() do
+    def get_results_from_es(nos_queries) do
         elastic_search_url = Application.get_env(:elasta_bot, ElastaBot.Query)[:elastic_search_url]
-        body = create_body_for_es_post_request()
+        body = create_body_for_es_post_request(nos_queries)
         HTTPotion.post elastic_search_url, [body: body,  headers: ["Content-Type": "application/json"]]
     end
 
@@ -17,11 +17,11 @@ defmodule ElastaBot.Query do
         Poison.Encoder.encode(map_to_convert, [])
     end
 
-    def create_body_for_es_post_request() do
+    def create_body_for_es_post_request(nos_queries \\ 10) do
         end_time =  :os.system_time(:seconds)*1000
-        start_time = end_time - 24 * 60 * 60 * 1000
+        start_time = end_time - 2 * 24 * 60 * 60 * 1000
         query = "source: ecom-cms AND environment: production AND (_forwarder_host: magnoliapublic2 OR _forwarder_host: magnoliapublic1) "
-        create_request(start_time, end_time, query, 10) 
+        create_request(start_time, end_time, query, nos_queries) 
         |> convert_to_json()
         |> to_string
     end

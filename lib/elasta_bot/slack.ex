4c9f51a9992c0984b028d7619222b36318c2e16b
mikemjharris <mike.mj.harris@gmail.com>
2016-12-04 18:31:28 +0000

    first go at pattern matching on messages - working but there's a todo to make more functional'

--- lib/elasta_bot/slack.ex
+++ lib/elasta_bot/slack.ex
@@ -6,7 +6,17 @@ defmodule ElastaBot.Slack do
     def handle_event(message = %{type: "message"}, slack, state) do
         # Only respond to messages to me!
         if Regex.run ~r/<@#{slack.me.id}>:?/, message.text do
-          message_res = ElastaBot.Query.query_es()
+
+          #TODO  this can be steamed better but it's working so gonna commit
+          message.text 
+          |> String.split()
+          |> case do
+            [_, "last", nos_queries , "alerts"] ->
+              IO.puts nos_queries
+              message_res = ElastaBot.Query.query_es(String.to_integer(nos_queries))
+            _ ->
+              message_res = "didn't match"
+            end
           send_message("<@#{message.user}> " <> message_res, message.channel, slack)
         end
         {:ok, state}

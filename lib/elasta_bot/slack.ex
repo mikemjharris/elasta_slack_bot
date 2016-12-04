c67fd750c3ed0dd31e8a4dd3ccfbdac8202ef851
mikemjharris <mike.mj.harris@gmail.com>
2016-12-04 18:44:51 +0000

    refactor streaming of message pattern matching

--- lib/elasta_bot/slack.ex
+++ lib/elasta_bot/slack.ex
@@ -6,18 +6,16 @@ defmodule ElastaBot.Slack do
     def handle_event(message = %{type: "message"}, slack, state) do
         # Only respond to messages to me!
         if Regex.run ~r/<@#{slack.me.id}>:?/, message.text do
-
-          #TODO  this can be steamed better but it's working so gonna commit
-          message.text 
+          return_message = message.text 
           |> String.split()
           |> case do
             [_, "last", nos_queries , "alerts"] ->
-              IO.puts nos_queries
-              message_res = ElastaBot.Query.query_es(String.to_integer(nos_queries))
+              ElastaBot.Query.query_es(String.to_integer(nos_queries))
             _ ->
-              message_res = "didn't match"
+              "didn't match"
             end
-          send_message("<@#{message.user}> " <> message_res, message.channel, slack)
+
+          send_message("<@#{message.user}> " <> return_message, message.channel, slack)
         end
         {:ok, state}
     end
